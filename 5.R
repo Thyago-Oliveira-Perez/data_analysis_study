ab5df0b72a9543442c449ea2d4fa357663093858
Thyago Oliveira Perez <thyagodoliveiraperez@gmail.com>
2024-05-07 21:40:21 -0700

    feat: activity 5

---
+++ 5.R
@@ -0,0 +1,17 @@
+# Apresente o gráfico de dispersão dos atributos, TotalSteps e TotalDistance; Faça a análise do gráfico;
+
+install.packages("magrittr")
+install.packages("dplyr")
+install.packages("ggplot2")
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+
+data <- read.csv("data/activity_data_heartrate.csv")
+
+plot(data$TotalSteps, data$TotalDistance,
+     main = "Grafico de Dispersao entre TotalSteps e Calorias",
+     xlab = "Total Steps",
+     ylab = "Distance",
+     col = "black",
+     pch = 2)
